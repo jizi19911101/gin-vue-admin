8c258f0670537a2a3ca199f3cf88a5b19f086ebc
vivi <menghua.chen@dreampix.cn>
2021-12-28 19:56:31 +0800

    增加git操作方法

--- server/service/api_test/api_testcase.go
+++ server/service/api_test/api_testcase.go
@@ -1,9 +1,11 @@
 package api_test
 
+import "github.com/jizi19911101/gin-vue-admin/server/utils"
+
 type ApiTestcaseService struct {
 }
 
 func (apiTestcaseService *ApiTestcaseService) ApiTestcaseCode() (err error) {
-
-	return err
+	err = utils.OsExecClone("  ", "https://git-ext.chumanapp.com/chuman-test/chuman-api-test-new")
+	return
 }
