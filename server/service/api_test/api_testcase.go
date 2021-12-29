c5ef58af3b29e13bd5dd73994977e74e40889741
vivi <menghua.chen@dreampix.cn>
2021-12-29 20:04:57 +0800

    解析模块成功！

--- server/service/api_test/api_testcase.go
+++ server/service/api_test/api_testcase.go
@@ -1,9 +1,11 @@
 package api_test
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/jizi19911101/gin-vue-admin/server/global"
+	"github.com/jizi19911101/gin-vue-admin/server/model/api_test"
 
 	"github.com/jizi19911101/gin-vue-admin/server/utils"
 )
@@ -23,10 +25,57 @@ func (apiTestcaseService *ApiTestcaseService) ApiTestcaseCode() (err error) {
 }
 
 func (apiTestcaseService *ApiTestcaseService) ParseApiTestcaseApi() (err error) {
+	var resModuleInfoList []api_test.ModuleInfo
+	delModuleInfoList := make([]uint, 0)
+	var count int64
+
+	// 解析出模块
 	pwd, _ := os.Getwd()
 	fileInfoList, err := ioutil.ReadDir(pwd + "/apiTestcaseCode/testcases")
-	for i := range fileInfoList {
-		fmt.Println(fileInfoList[i].Name()) //打印当前文件或目录下的文件或目录名
+	if len(fileInfoList) == 0 {
+		return
 	}
+	moduleInfoList := make([]api_test.ModuleInfo, 0)
+	moduleInfoListMap := make(map[string]api_test.ModuleInfo)
+	for i := range fileInfoList {
+		fileName := fileInfoList[i].Name()
+		moduleInfoListMap[fileName] = api_test.ModuleInfo{
+			Name: fileName,
+		}
+	}
+
+	// 查库查出模块
+	db := global.GVA_DB.Model(&api_test.ModuleInfo{})
+	db.Find(&resModuleInfoList).Count(&count)
+
+	// 把增量模块插入库
+	if count == 0 {
+		for _, v := range moduleInfoListMap {
+			moduleInfoList = append(moduleInfoList, v)
+		}
+		db.Create(&moduleInfoList)
+		return
+	} else {
+		for _, v := range resModuleInfoList {
+			_, ok := moduleInfoListMap[v.Name]
+			if ok {
+				delete(moduleInfoListMap, v.Name)
+			} else {
+				delModuleInfoList = append(delModuleInfoList, v.ID)
+			}
+		}
+	}
+
+	if len(moduleInfoListMap) != 0 {
+		for _, v := range moduleInfoListMap {
+			moduleInfoList = append(moduleInfoList, v)
+		}
+		db.Create(&moduleInfoList)
+	}
+
+	if len(delModuleInfoList) != 0 {
+		db.Delete(&api_test.ModuleInfo{}, delModuleInfoList)
+	}
+
 	return
 }
