3e497016acfcc78dcbb9863f95de0e2a9658abc1
vivi <menghua.chen@dreampix.cn>
2021-12-29 14:47:59 +0800

    拉取代码接口调通

--- server/service/api_test/api_testcase.go
+++ server/service/api_test/api_testcase.go
@@ -1,11 +1,21 @@
 package api_test
 
-import "github.com/jizi19911101/gin-vue-admin/server/utils"
+import (
+	"os"
+
+	"github.com/jizi19911101/gin-vue-admin/server/utils"
+)
 
 type ApiTestcaseService struct {
 }
 
 func (apiTestcaseService *ApiTestcaseService) ApiTestcaseCode() (err error) {
-	err = utils.OsExecClone("  ", "https://git-ext.chumanapp.com/chuman-test/chuman-api-test-new")
+	folder, _ := os.Getwd()
+	targetFolder := folder + "/apiTestcaseCode"
+	if err, _ := os.Stat(targetFolder); err != nil {
+		os.Mkdir(targetFolder, 755)
+	}
+
+	err = utils.OsExecClone(targetFolder, "https://git-ext.chumanapp.com/chuman-test/chuman-api-test-new")
 	return
 }
