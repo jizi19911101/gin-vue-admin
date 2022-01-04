2a0c70a78367e4bd1c12825c1ea8121635af1cbb
vivi <menghua.chen@dreampix.cn>
2022-01-04 18:45:38 +0800

    InitSyncRouter不用middleware.OperationRecord()

--- server/router/sync/sync.go
+++ server/router/sync/sync.go
@@ -3,14 +3,14 @@ package sync
 import (
 	"github.com/gin-gonic/gin"
 	v1 "github.com/jizi19911101/gin-vue-admin/server/api/v1"
-	"github.com/jizi19911101/gin-vue-admin/server/middleware"
 )
 
 type SyncRouter struct {
 }
 
 func (s *SyncRouter) InitSyncRouter(Router *gin.RouterGroup) {
-	syncRouter := Router.Group("sync").Use(middleware.OperationRecord())
+	//syncRouter := Router.Group("sync").Use(middleware.OperationRecord())
+	syncRouter := Router.Group("sync")
 	//apiTestcaseRouterWithRecord := Router.Group("apiTestcase")
 	var syncApi = v1.ApiGroupApp.SyncGroup
 	{
