8eff2192e2e88e994ac4acd031969cf3d2c2fc86
vivi <menghua.chen@dreampix.cn>
2022-01-25 18:12:06 +0800

    写了第二条自测用例

--- server/apitest/sync/sync_test.go
+++ server/apitest/sync/sync_test.go
@@ -48,3 +48,28 @@ func TestSyncApiTestCase(t *testing.T) {
 	obj.Value("msg").Equal("同步并解析接口自动化代码成功！")
 
 }
+
+func TestSyncApiTestReport(t *testing.T) {
+	//engine := gin.New()
+	handler := initialize.Routers()
+
+	e := httpexpect.WithConfig(httpexpect.Config{
+		Client: &http.Client{
+			Transport: httpexpect.NewBinder(handler),
+			Jar:       httpexpect.NewJar(),
+		},
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
+
+	obj := e.GET("/sync/syncApiTestReport").
+		WithQuery("name", "百度").
+		WithQuery("url", "https://www.baidu.com/").
+		Expect().
+		Status(http.StatusOK).JSON().Object()
+
+	obj.Value("msg").Equal("同步接口测试报告成功！")
+
+}
